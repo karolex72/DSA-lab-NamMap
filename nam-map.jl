bd13aa4cd889679315735effc8e897ca0b0347ca
karolex72 <71222315+karolex72@users.noreply.github.com>
2022-10-09 20:16:59 +0200

    packages...

---
+++ nam-map.jl
@@ -0,0 +1,4 @@
+#-29.572219:10.711252 , -16.382026:10.711252,
+coast(region="270/20/305/25+r", proj=(name=:omercp, center=[280 25.5], parallels=[22 69]),
+      frame=:ag, res=:i, area=250, shore=:thinnest, land=:burlywood, water=:azure,
+      rose="jTR+w1+f2+l+o0.4", figsize=12, par=(FONT_TITLE=8, MAP_TITLE_OFFSET=0.12), show=true)
